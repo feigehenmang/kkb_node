1d2d335a6ad019d8db7ffb576c92460624f7d985
lijinfei <1638546595@qq.com>
2021-05-30 17:21:32 +0800

    test

--- cli/test/src/views/a.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="a">
-        <h1>This is an a page</h1>
-    </div>
-</template>
--- cli/test/template/layout.vue.js
+++
@@ -1,5 +0,0 @@
-return `<template>
-    <div class="${name}">
-        <h1>This is an ${name} page</h1>
-    </div>
-</template>`
